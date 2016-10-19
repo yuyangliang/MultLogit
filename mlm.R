2d1df822599a03aec9681212a76282a3f139d5bb
yuyangliang <mydoraemons@hotmail.com>
2016-10-18 22:50:58 -0400

    Modeling

--- mlm.R
+++ mlm.R
@@ -1,11 +1,13 @@
 library('DBI')
 
-# connect to the database
+source('dbk.R')
+
+#connect to the database
 c =  dbConnect(RMySQL::MySQL(), 
-               username = 'josh', 
-               password = 'pickles5000!',
-               host = 'opendata.missouri.edu', 
-               dbname = 'WEBMD_2016'
+               username = u, 
+               password = p,
+               host = h, 
+               dbname = d
 )
 
 flist = list.files(path = './Classification Result', pattern = '.csv')
@@ -14,14 +16,14 @@ dir = './Classification Result/'
 for (i in 1:length(flist)) f[[i]] = read.csv(file = paste0(dir, flist[i]), header = T, stringsAsFactors = F)
 for (i in 1:length(flist)) f[[i]]$forum = sub("^([^.]*).*", "\\1", flist[i])
 
-#Obtain inferred_replies
+#obtain inferred_replies
 ptdc = function(tbl){
   sql = sprintf("SELECT uniqueID, inferred_replies FROM %s", tbl)
   t = dbGetQuery(c, sql)
   return(t)
 }
-  
-#Manipulate the dataset
+
+#manipulate the dataset
 rcnt = function(f1){
   for (i in 1:nrow(f1)){
     f1$type[i] = paste0(f1[i,9], f1[i,10], f1[i,11], f1[i,12], f1[i,13])
@@ -30,7 +32,7 @@ rcnt = function(f1){
   f1 = f1[order(f1$qid, f1$localID, f1$datetime), ]
   if (nrow(f1) > 0 ){
     f1$status = 0
-    f1$status[f1$type == '00000'] = 1    
+    f1$status[f1$type == '00000'] = 1
     f1$status[f1$type == '10000'] = 2
     f1$status[f1$type == '01000'] = 3
     f1$status[f1$type == '00100'] = 4
@@ -51,16 +53,18 @@ rcnt = function(f1){
       f3 = subset(f2, poster == plist[k])
       f4 = subset(f3, select = c(qid, poster, datetime, status))
       f4 = f4[order(f4$datetime), ] #a list of posts created by the poster 
+      
       names(f4) = c('qid', 'poi', 'poidate', 'status')
       rownames(f4) = NULL
       if (nrow(f4) > 1){#for each post, obtain a list of posts from the whole forum that came before the post
-        f4$tdif = c(0, log(as.numeric(f4$poidate[2:nrow(f4)]) - as.numeric(f4$poidate[1:(nrow(f4)-1)]) + 1))
+        #f4$tdif = c(0, log(as.numeric(f4$poidate[2:nrow(f4)]) - as.numeric(f4$poidate[1:(nrow(f4)-1)]) + 1))
+        f4$month = cut(f4$poidate, 'month')
         f5 = subset(f2, f2$datetime < f4$poidate[1])
         f51 = table(factor(f5$status, levels = 2:8))
         for (l in 2:nrow(f4)){
           f6 = subset(f2, (f2$datetime < f4$poidate[l]) & (f2$datetime > f4$poidate[l-1]))
           f61 = table(factor(f6$status, levels = 2:8))
-          f51 = rbind(f51, f61)        
+          f51 = rbind(f51, f61)
         }
         rownames(f51) = NULL
         colnames(f51) = seq(2, 8)
@@ -102,5 +106,8 @@ m = MCMCglmm(status ~ -1 + X2 + X3 + X4 + X5 + X6 + X7 + X8 + tdif + trait,
 out = summary(m)
 out.1 = matrix(nrow = 1, ncol = 16)
 out.1[1, ] = c(out$solutions[,1], out$Gcovariances[,1], out$Rcovariances[1,1])
-colnames(out.1) = c('C', 'RI', 'RE', 'PE', 'PI', 'IE', 'CE', 'TD', 'C2V1', 'C3V1', 'C4V1', 'C5V1', 'C6V1', 'C7V1', 'GV', 'RV')
+colnames(out.1) = c('C', 'RI', 'RE', 'PE', 'PI', 'IE', 'CE', 'TD', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'GV', 'RV')
 write.csv(out.1, file = 'paras.csv', row.names = F)
+
+t = aggregate(c(cnth[,1:7]), list(cnth$poi, cnth$month), sum)
+t = reshape(data.frame(cnth[, 8:11], value = 1), idvar = c('poi', 'poidate', 'qid'), timevar = 'status', direction = 'wide')
